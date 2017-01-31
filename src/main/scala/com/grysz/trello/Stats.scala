b964dfac3b278f966c6319b8dd974afb9bbd4078
Marcin Gryszko <marcin@grysz.com>
2017-01-31 07:25:50 +0100

    Separate raw API access from statistics

--- src/main/scala/com/grysz/trello/Stats.scala
+++ src/main/scala/com/grysz/trello/Stats.scala
@@ -1,50 +1,14 @@
 package com.grysz.trello
 
-import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.model.StatusCodes.OK
-import akka.http.scaladsl.model.Uri.Query
-import akka.http.scaladsl.model.{HttpRequest, ResponseEntity, Uri}
-import akka.http.scaladsl.unmarshalling.{Unmarshal, Unmarshaller}
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Sink
-import spray.json.{DefaultJsonProtocol, RootJsonFormat}
-
 import scala.concurrent.{ExecutionContext, Future}
-
-case class TrelloList(id: String, name: String)
-
-case class TrelloCard(id: String, name: String, idList: String)
 
 case class StatsBoard(lists: Seq[StatsList])
 
 case class StatsList(name: String, numCards: Int)
 
-trait JsonProtocol extends DefaultJsonProtocol with SprayJsonSupport {
-  implicit val listProtocol: RootJsonFormat[TrelloList] = jsonFormat2(TrelloList)
-  implicit val cardProtocol: RootJsonFormat[TrelloCard] = jsonFormat3(TrelloCard)
-}
-
-object Api {
-  def apply(key: String, token: String)(implicit actorSystem: ActorSystem = ActorSystem("TrelloStats")) = new Api(key, token)(actorSystem)
-}
-
-class Api(key: String, token: String)(implicit actorSystem: ActorSystem) extends JsonProtocol {
-  private val endpoint = Uri("https://api.trello.com")
-  private val authParams = Map("key" -> key, "token" -> token)
-  private implicit val actorMaterializer = ActorMaterializer()
-
-  def openLists(idBoard: String)(implicit ec: ExecutionContext): Future[Seq[TrelloList]] = {
-    request[Seq[TrelloList]](s"/1/boards/$idBoard/lists/open")
-  }
-
-  def openCards(idBoard: String)(implicit ec: ExecutionContext): Future[Seq[TrelloCard]] = {
-    request[Seq[TrelloCard]](s"/1/boards/$idBoard/cards/open")
-  }
-
-  def openListsWithCards(idBoard: String) (implicit ec: ExecutionContext): Future[StatsBoard] = {
-    val cardsAndLists = openCards(idBoard) zip openLists(idBoard)
+class Stats(api: Api) {
+  def openListsWithCards(idBoard: String)(implicit ec: ExecutionContext): Future[StatsBoard] = {
+    val cardsAndLists = api.openCards(idBoard) zip api.openLists(idBoard)
     cardsAndLists.flatMap { case (cards, lists) =>
       val statsLists = lists.map(l => StatsList(l.name, countCardsOfList(cards, l.id)))
       Future.successful(StatsBoard(statsLists))
@@ -52,22 +16,4 @@ class Api(key: String, token: String)(implicit actorSystem: ActorSystem) extends
   }
 
   private def countCardsOfList(cards: Seq[TrelloCard], idList: String) = cards.count(_.idList == idList)
-
-  private def request[T](path: String, params: Map[String, String] = Map())(implicit ec: ExecutionContext, unmarshaller: Unmarshaller[ResponseEntity, T]): Future[T] = {
-    Http().singleRequest(HttpRequest(uri = uri(path, params))).flatMap(resp => {
-      resp.status match {
-        case OK => Unmarshal(resp.entity).to[T]
-        case _ =>
-          val status = resp.status
-          discardEntity(resp.entity)
-          Future.failed(new RuntimeException(s"Request failed with status ${status.reason}"))
-      }
-    })
-  }
-
-  private def uri(path: String, params: Map[String, String]) = endpoint
-    .withPath(Uri.Path(path))
-    .withQuery(Query(authParams ++ params))
-
-  private def discardEntity(entity: ResponseEntity) = entity.dataBytes.runWith(Sink.ignore)
 }
--- src/test/scala/com/grysz/trello/ApiTest.scala
+++ src/test/scala/com/grysz/trello/ApiTest.scala
@@ -4,25 +4,25 @@ import akka.actor.ActorSystem
 import com.typesafe.config.ConfigFactory
 import org.scalatest.{FlatSpec, Inspectors, Matchers}
 
-import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 
-class StatsTest extends FlatSpec with Matchers with Inspectors {
+class ApiTest extends FlatSpec with Matchers with Inspectors {
   val config = ConfigFactory.load()
   implicit private val actorSystem = ActorSystem("TrelloApiIntegrationTests", config)
   import actorSystem.dispatcher
 
-  "Trello stats" should "get board lists and cards" in {
-    val board = result(() => stats.openListsWithCards(idBoard))
-    board.lists should not be empty
-    forAtLeast(1, board.lists) { list =>
-      list.name should not be empty
-      list.numCards should be >= 1
-    }
+  "Trello API" should "get board open lists" in {
+    val lists = result(() => api.openLists(idBoard))
+    lists should not be empty
+  }
+
+  it should "get board open cards" in {
+    val cards = result(() => api.openCards(idBoard))
+    cards should not be empty
   }
 
   val api = Api(config.getString("trello.key"), config.getString("trello.token"))
-  val stats = new Stats(api)
 
   val idBoard = "5783d18ebed64e477bda0535"
 
