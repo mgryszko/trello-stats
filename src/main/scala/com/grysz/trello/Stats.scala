dbe65b5a86b63413cc38fc031b85905c823f111b
Marcin Gryszko <marcin@grysz.com>
2017-02-17 06:46:20 +0100

    Weaken Monad to Applicative

--- src/main/scala/com/grysz/trello/Stats.scala
+++ src/main/scala/com/grysz/trello/Stats.scala
@@ -1,32 +1,30 @@
 package com.grysz.trello
 
-import scalaz.Monad
+import scalaz.Applicative
 
 case class StatsBoard(lists: Seq[StatsList])
 
 case class StatsList(name: String, numCards: Int)
 
 trait Stats[P[_]] {
-  import scalaz.syntax.monad._
+  import scalaz.syntax.applicative._
 
-  implicit val M: Monad[P]
+  implicit val A: Applicative[P]
   val api: Api[P]
 
   def openListsWithCards(idBoard: String): P[StatsBoard] = {
-    api.openCards(idBoard) >>= { cards =>
-      api.openLists(idBoard) >>= { lists =>
-        val statsLists = lists.map(l => StatsList(l.name, countCardsOfList(cards, l.id)))
-        StatsBoard(statsLists).point
-      }
-    }
+    (api.openCards(idBoard) |@| api.openLists(idBoard))((cards, lists) => {
+      val statsLists = lists.map(l => StatsList(l.name, countCardsOfList(cards, l.id)))
+      StatsBoard(statsLists)
+    })
   }
 
   private def countCardsOfList(cards: Seq[TrelloCard], idList: String) = cards.count(_.idList == idList)
 }
 
 object Stats {
-  def apply[P[_]: Monad: Api] = new Stats[P] {
-    val M: Monad[P] = implicitly
+  def apply[P[_]: Applicative: Api] = new Stats[P] {
+    val A: Applicative[P] = implicitly
     val api: Api[P] = implicitly
   }
 }
