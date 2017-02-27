66799124d80ae4ef97fcc76ded5a7076cb378738
Marcin Gryszko <marcin@grysz.com>
2017-02-27 06:53:35 +0100

    Add an async test of card stats

--- src/test/scala/com/grysz/trello/AsyncStatsTest.scala
+++ src/test/scala/com/grysz/trello/AsyncStatsTest.scala
@@ -1,5 +1,7 @@
 package com.grysz.trello
 
+import java.time.Duration
+
 import akka.actor.ActorSystem
 import com.typesafe.config.ConfigFactory
 import org.scalatest.{FlatSpec, Inspectors, Matchers}
@@ -19,6 +21,7 @@ class AsyncStatsTest extends FlatSpec with Matchers with Inspectors {
   val stats = Stats[Future]
 
   val idBoard = "5783d18ebed64e477bda0535"
+  val idCard = "57f7b542839dd203cf551704"
 
   "Trello stats" should "get board lists and cards" in {
     val numCardsByList = result(() => stats.numCardsByList(idBoard))
@@ -27,6 +30,16 @@ class AsyncStatsTest extends FlatSpec with Matchers with Inspectors {
     forAtLeast(1, numCardsByList.toSeq) { case (name, numCards) =>
       name should not be empty
       numCards should be >= 1
+    }
+  }
+
+  it should "calculate how much time did a card spent in every list" in {
+    val timesByList = result(() => stats.timeSpentInLists(idCard))
+
+    timesByList should not be empty
+    forAll(timesByList.toSeq) { case (idList, time) =>
+      idList should not be empty
+      time should be > Duration.ZERO
     }
   }
 
