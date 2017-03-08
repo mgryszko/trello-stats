cf6a400fd43cf986581a8e95188f6f54f800a382
Marcin Gryszko <marcin@grysz.com>
2017-03-08 07:28:16 +0100

    Remove meaningless words

---
+++ src/main/scala/com/grysz/trello/Main.scala
@@ -0,0 +1,48 @@
+package com.grysz.trello
+
+import akka.actor.ActorSystem
+import com.typesafe.config.ConfigFactory
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+import scalaz.Monad
+
+object Main {
+  case class Config(idCard: String)
+
+  def main(args: Array[String]): Unit = {
+    val parser = new scopt.OptionParser[Config]("trello-stats") {
+      help("help").text("prints this usage text")
+
+      arg[String]("<idCard>").required().action((idCard, c) => c.copy(idCard = idCard)).text("idCard")
+    }
+
+    parser.parse(args, Config(idCard = "")) match {
+      case Some(config) => {
+        Cli.timeSpentInLists(config.idCard)
+      }
+
+      case None =>
+    }
+  }
+}
+
+object Cli {
+  private val config = ConfigFactory.load()
+  implicit private val actorSystem = ActorSystem("TrelloApi", config)
+  import actorSystem.dispatcher
+
+  implicit private val api = AsyncApi(config.getString("trello.key"), config.getString("trello.token"))
+
+  import scalaz.std.scalaFuture
+  implicit private val monadFuture: Monad[Future] = scalaFuture.futureInstance
+  private val stats = Stats[Future]
+
+  import Formatter._
+  import scalaz.syntax.show._
+
+  def timeSpentInLists(idCard: String): Unit = println(result(() => stats.timeSpentInLists(idCard)).shows)
+
+  private def result[T](asynchOp: () => Future[T]): T = Await.result(asynchOp(), 10 seconds)
+}
+
