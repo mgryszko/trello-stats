b6cd2743280d6d8a3bf264649400bb1386823570
Marcin Gryszko <marcin@grysz.com>
2017-03-15 07:36:07 +0100

    Calculate average duration contained in sequences and maps

---
+++ src/main/scala/com/grysz/trello/AvgDuration.scala
@@ -0,0 +1,12 @@
+package com.grysz.trello
+
+import java.time.Duration
+
+object AvgDuration {
+  def avg[A](m: Map[A, List[Duration]]): Map[A, Duration] = m mapValues avg
+
+  private def avg(xs: List[Duration]): Duration = {
+    val total = xs.fold(Duration.ZERO)(_ plus _)
+    total dividedBy xs.size
+  }
+}
---
+++ src/test/scala/com/grysz/trello/AvgDurationTest.scala
@@ -0,0 +1,36 @@
+package com.grysz.trello
+
+import java.time.Duration
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class AvgDurationTest extends FlatSpec with Matchers {
+
+  val timesSpentByList = Map(
+    "list1" -> List(Duration.parse("PT64H37M21.316S"), Duration.parse("PT33H3M48.724S"), Duration.parse("PT4828H14M30.461S")),
+    "list2" -> List(Duration.parse("PT309H38M33.444S"), Duration.parse("PT59H4M41.737S")),
+    "list3" -> List(Duration.parse("PT23H52M56.719S")),
+    "list4" -> List(Duration.parse("PT102H36M12.341S"), Duration.parse("PT870H4M27.923S"), Duration.parse("PT1201H22M3.175S")),
+    "list5" -> List(Duration.parse("PT318H22M24.418S"), Duration.parse("PT21H34M7.931S")),
+    "list6" -> List(Duration.parse("PT134H30M32.215S")),
+    "list7" -> List(Duration.parse("PT645H11M56.453S"))
+  )
+
+  val expectedAvgTimeSpentByList = Map(
+    "list1" -> Duration.parse("PT1641H58M33S"),
+    "list2" -> Duration.parse("PT184H21M37S"),
+    "list3" -> Duration.parse("PT23H52M56S"),
+    "list4" -> Duration.parse("PT724H40M54S"),
+    "list5" -> Duration.parse("PT169H58M16S"),
+    "list6" -> Duration.parse("PT134H30M32S"),
+    "list7" -> Duration.parse("PT645H11M56S")
+  )
+
+  it should "calculate average duration" in {
+    AvgDuration.avg(timesSpentByList).mapValues(DurationOps.stripNano(_)) should equal(expectedAvgTimeSpentByList)
+  }
+
+  object DurationOps {
+    def stripNano(d: Duration) = Duration.ofSeconds(d.getSeconds)
+  }
+}
