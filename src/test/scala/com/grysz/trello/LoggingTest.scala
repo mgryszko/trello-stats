9ce33ec59a6e56190392dec58e932de2b40e366c
Marcin Gryszko <marcin@grysz.com>
2017-05-15 08:04:40 +0200

    WIP Greeter encapsulated

--- src/test/scala/com/grysz/trello/LoggingTest.scala
+++ src/test/scala/com/grysz/trello/LoggingTest.scala
@@ -2,7 +2,21 @@ package com.grysz.trello
 
 import org.scalatest.{FlatSpec, Matchers}
 
-import scalaz.{MonadReader, MonadTell, ReaderWriterState}
+import scalaz.effect.IO
+import scalaz.{Monad, MonadReader, MonadTell, ReaderWriterState}
+
+trait Greeter[P[_]] {
+  implicit val M: Monad[P]
+  val RD: MonadReader[P, String]
+  val WR: MonadTell[P, String]
+
+  import scalaz.syntax.monad._
+
+  def greet: P[String] = for {
+    greeting <- RD.ask
+    _ <- WR.tell(s"greeting $greeting")
+  } yield greeting
+}
 
 class LoggingTest extends FlatSpec with Matchers {
   type Program[A] = ReaderWriterState[String, String, Unit, A]
@@ -12,14 +26,25 @@ class LoggingTest extends FlatSpec with Matchers {
   val MR = MonadReader[Program, String]
   val MW = MonadTell[Program, String]
 
-  def greet: Program[String] = for {
-    greeting <- MR.ask
-    _ <- MW.tell(s"greeting $greeting")
-  } yield s"Hello $greeting"
+  val MWIO = new MonadTell[IO, String] {
 
+    import scalaz.syntax.monad._
+
+    def writer[A](w: String, v: A): IO[A] = IO.putStrLn(w) >> point(v)
+
+    def point[A](a: => A): IO[A] = IO(a)
+
+    def bind[A, B](fa: IO[A])(f: (A) => IO[B]): IO[B] = fa flatMap f
+  }
+
+  val greeter: Greeter[Program] = new Greeter[Program] {
+    val M: Monad[Program] = Monad[Program]
+    val RD: MonadReader[Program, String] = MR
+    val WR: MonadTell[Program, String] = MW
+  }
 
   it should "greet and log" in {
-    val (log, _) = greet.eval("Marcin", ())
+    val (log, _) = greeter.greet.eval("Marcin", ())
 
     log should equal("greeting Marcin")
   }
