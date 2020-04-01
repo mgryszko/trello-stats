d8e2a1409a2120c2f8aea8d09bbdd3986a5fdeb6
mgryszko <marcin.gryszko@lastminute.com>
2020-04-01 13:23:12 +0200

    Test modified - forgot about the reason

--- src/test/scala/com/grysz/trello/LoggingTest.scala
+++ src/test/scala/com/grysz/trello/LoggingTest.scala
@@ -3,17 +3,20 @@ package com.grysz.trello
 import org.scalatest.{FlatSpec, Matchers}
 
 import scalaz.effect.IO
-import scalaz.{Monad, MonadTell}
+import scalaz.{Monad, MonadReader, MonadTell, MonadTrans, ReaderT}
 
 trait Greeter[P[_]] {
   implicit val M: Monad[P]
+  val RD: MonadReader[P, String]
   val WR: MonadTell[P, String]
 
   import scalaz.syntax.monad._
 
-  def greet(greeting: String): P[String] = {
-    val g = s"greeting $greeting"
-    WR.tell(g) >> M.point(g)
+  def greet(): P[String] = {
+    RD.ask >>= { greeting =>
+      val g = s"greeting $greeting"
+      WR.tell(g) >> M.point(g)
+    }
   }
 }
 
@@ -21,22 +24,45 @@ class LoggingTest extends FlatSpec with Matchers {
   import scalaz.syntax.monad._
   import scalaz.effect.IO._
 
-  val MW = new MonadTell[IO, String] {
+  type Program[A] = ReaderT[IO, String, A]
 
-    def writer[A](w: String, v: A): IO[A] = IO.putStrLn(w) >> point(v)
+  val MRIO = new MonadReader[IO, String] {
+    def ask: IO[String] = IO.readLn
+
+    def local[A](f: String => String)(fa: IO[A]): IO[A] = ???
+
+    def point[A](a: => A): IO[A] = IO(a)
+
+    def bind[A, B](fa: IO[A])(f: (A) => IO[B]): IO[B] = fa.flatMap(f)
+  }
+
+  val MWIO = new MonadTell[IO, String] {
+    def writer[A](w: String, v: A): IO[A] = IO.putStrLn(w) >> IO(v)
 
     def point[A](a: => A): IO[A] = IO(a)
 
     def bind[A, B](fa: IO[A])(f: (A) => IO[B]): IO[B] = fa flatMap f
   }
 
-  val greeter: Greeter[IO] = new Greeter[IO] {
-    implicit val M: Monad[IO] = Monad[IO]
-    val WR: MonadTell[IO, String] = MW
+  val MW = new MonadTell[ReaderT[IO, String, ?], String] {
+    def writer[A](w: String, v: A): ReaderT[IO, String, A] =
+      MonadTrans[ReaderT[?[_], String, ?]].liftM(MWIO.writer(w, v))
+
+    def point[A](a: => A): ReaderT[IO, String, A] =
+      MonadTrans[ReaderT[?[_], String, ?]].liftM(MWIO.point(a))
+
+    def bind[A, B](fa: ReaderT[IO, String, A])(f: A => ReaderT[IO, String, B]): ReaderT[IO, String, B] =
+      fa.flatMap(f)
+  }
+
+  val greeter: Greeter[ReaderT[IO, String, ?]] = new Greeter[ReaderT[IO, String, ?]] {
+    implicit val RD: MonadReader[ReaderT[IO, String, ?], String] = MonadReader[ReaderT[IO, String, ?], String]
+    implicit val M: Monad[ReaderT[IO, String, ?]] = Monad[ReaderT[IO, String, ?]]
+    val WR: MonadTell[ReaderT[IO, String, ?], String] = MW
   }
 
   it should "greet and log" in {
-    val log = greeter.greet("Marcin").unsafePerformIO()
+    val log = greeter.greet.run("Marcin").unsafePerformIO()
     log should equal("greeting Marcin")
   }
 }
